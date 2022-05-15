48a2d921ac545cbe21cc5d361c2480eb6dbf84db
abdulrahmanabdullah <nfs056@gmail.com>
2022-05-15 16:01:23 +0300

    init repo, create client and server folders, create .gitignore file, install deps on server and client, connect to mongoose database

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser  from 'body-parser';
+import mongoose from 'mongoose';
+
+// create app 
+const app = express();
+
+//constent 
+const PORT = process.env.PORT || 5000; 
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.afxdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ;
+app.use(cors());
+app.use(bodyParser.json({limit:'30mb', extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
+
+//Connection 
+mongoose.connect(CONNECTION_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+   });
+const connection = mongoose.connection ;
+
+connection.on('err', (err)=>{
+    console.log(`mongoose throw this :: ${err}`);
+})
+
+// Fire server 🔥
+app.listen(PORT, console.log(`server running on ${PORT}`));
