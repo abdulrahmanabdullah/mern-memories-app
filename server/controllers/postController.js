247be09e3f7137b0e5e97539b33adc6010a30bb7
abdulrahmanabdullah <nfs056@gmail.com>
2022-05-20 18:04:05 +0300

    create update post method in backend

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -4,7 +4,7 @@ import PostMessage from "../models/postSchem";
 export const getPosts = async (req, res) => {
   try {
     const collection = await PostMessage.find();
-    res.status(200).json(collection);
+    return res.status(200).json(collection);
   } catch (error) {
     console.log(error.message);
   }
@@ -24,6 +24,28 @@ export const createPost = async (req, res) => {
     res.status(201).json(newPost);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+//update post
+export const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { creator, title, message, selectedFile, tags } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No post with this Id");
+    const updateValues = {
+      creator,
+      title,
+      message,
+      selectedFile,
+      tags,
+      _id: id,
+    };
+    await PostMessage.findByIdAndUpdate(id, updateValues, { new: true });
+    res.status(202).json(updateValues);
+  } catch (error) {
+    res.status(406).json({ message: error.message });
   }
 };
 
